8bd3717f998f746d8199ab364cf5e46a6de9178e
S63781 <54980743+S63781@users.noreply.github.com>
2025-03-01 08:10:30 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,9 +1,9 @@
 const settings = {
   packname: 'Knight',
   author: 'Bot',
-  botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '917023951514', //Your number
+  botName: "Sunfeast-Melody Bot",
+  botOwner: 'Sunfeast Melody', // Your name
+  ownerNumber: '917363997539', //Your number
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
